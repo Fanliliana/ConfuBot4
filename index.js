f4680f8ffbac0271a4ddac8ee42c072d7680f819
Liliana <94010909+GataDios@users.noreply.github.com>
2022-01-20 19:32:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1627,7 +1627,7 @@ ${anu.result.url_video}
           yta(mulaikah).then((res) => {
             const { dl_link, thumb, title, filesizeF, filesize } = res;
             axios
-              .get(`https://api.lolhuman.xyz/api/ytplay?apikey=56c3f2f2254d87b84051ab78&{dl_link}`)
+              .get(`https://api.lolhuman.xyz/api/ytplay?q=${play}apikey=${zeksapi}{dl_link}`)
               .then(async (a) => {
                 if (Number(filesize) >= 9999999)
                   return sendMediaURL(
